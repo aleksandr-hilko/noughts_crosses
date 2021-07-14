105b5f62e34a49eaf9bc0656e24a4b5c5b173bc9
aleksandr-hilko <aleksandr.hilko@mail.ru>
2021-07-14 22:45:50 +0300

    Add login and register views

--- app/app.py
+++ app/app.py
@@ -1,14 +1,21 @@
 import os
-from flask import Flask, request, jsonify
+
+from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, create_access_token
 from flask_pymongo import PyMongo
 
-
 application = Flask(__name__)
+jwt = JWTManager(application)
+
+application.config["JWT_SECRET_KEY"] = "this-is-secret-key"
+
 
 application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
 
 mongo = PyMongo(application)
 db = mongo.db
+
+user = db["User"]
 
 @application.route('/')
 def index():
@@ -16,6 +23,40 @@ def index():
         status=True,
         message='Welcome to the Dockerized Flask MongoDB app!'
     )
+
+
+@application.route("/register", methods=["POST"])
+def register():
+    email = request.form["email"]
+    # test = User.query.filter_by(email=email).first()
+    test = user.find_one({"email": email})
+    if test:
+        return jsonify(message="User Already Exist"), 409
+    else:
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        password = request.form["password"]
+        user_info = dict(first_name=first_name, last_name=last_name, email=email, password=password)
+        user.insert_one(user_info)
+        return jsonify(message="User added sucessfully"), 201
+
+
+@application.route("/login", methods=["POST"])
+def login():
+    if request.is_json:
+        email = request.json["email"]
+        password = request.json["password"]
+    else:
+        email = request.form["email"]
+        password = request.form["password"]
+
+    test = user.find_one({"email": email, "password": password})
+    if test:
+        access_token = create_access_token(identity=email)
+        return jsonify(message="Login Succeeded!", access_token=access_token), 201
+    else:
+        return jsonify(message="Bad Email or Password"), 401
+
 
 if __name__ == "__main__":
     ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
